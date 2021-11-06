65ee755a78a62acb377e145e462fbdf20ca3ec4f
romiooo <romimahato1416@.com>
2021-11-06 17:58:33 +0530

    Display DAta from shop

--- src/Shop.js
+++ src/Shop.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
-function Shop () {
+function Shop() {
+
+  useEffect(()=>{
+    fetchItems();
+  },[]);
+
+  const [items, setItems]= useState([]);
+
+  const fetchItems=async()=>{
+    const data=await fetch('https://fortnite-api.theapinetwork.com/upcoming/get');
+
+    const items = await data.json();
+    console.log(items.data);
+    setItems(items.data);
+  }
+
   return (
     <div className="Shop-Page">
-        <h1>Shop Page</h1>
+      <h1>Shop Page</h1>
+        {items.map(item => {
+          {console.log(item)}
+          return <h1 key={item.itemId}>{item.item.name}</h1>
+})}
     </div>
   );
 }
